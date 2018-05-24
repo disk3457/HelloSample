09d98864ba9aed39d2a403e9b2ee5e8bb01ecd8c
disk3457 <dsakkyan0304@yahoo.co.jp>
2018-05-24 10:19:10 +0900

    5月24日

--- app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
+++ app/src/main/java/com/websarva/wings/android/hellosample/HelloSampleActivity.java
@@ -2,6 +2,10 @@ package com.websarva.wings.android.hellosample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HelloSampleActivity extends AppCompatActivity {
 
@@ -9,5 +13,29 @@ public class HelloSampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_sample);
+
+        Button btClick = findViewById(R.id.btClick);
+
+        HelloListener listener = new HelloListener();
+
+        btClick.setOnClickListener(listener);
+    }
+
+    private class HelloListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+
+            EditText input = findViewById(R.id.etName);
+
+            TextView output = findViewById(R.id.tvOutput);
+
+            String inputStr = input.getText().toString();
+
+            output.setText(inputStr + "さん、こんにちは！");
+
+
+        }
     }
 }
+
